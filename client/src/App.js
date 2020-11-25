0f29fe3ea77ed722eac2ed992d9198cf3d76c01e
DKosVd <dkosyhiyan@gmail.com>
2020-11-25 19:19:38 +0500

    404

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { Redirect, Route } from 'react-router-dom'; 
+import { Redirect, Route, Switch } from 'react-router-dom'; 
 import About from './pages/About';
 import MainPage from './pages/MainPage';
 import Services from './pages/Services';
 import Contacts from './pages/Contacts';
 import Login from './pages/Login';
+import Notfound from './components/NotFound';
 
 function App() {
 
   return (
     <div className="wrapper">
+      <Switch>
         <Route exact path='/main' component={MainPage}/>
         <Route exact path='/services' component={Services}/>
         <Route exact path='/about' component={About}/>
         <Route exact path='/contacts' component={Contacts}/>
         <Route exact path='/login' component={Login}/>
+        <Route path="/*" component={Notfound}/>
         <Redirect from='/' to='/main'></Redirect>
+      </Switch>
     </div>
   );
 }
---
+++ client/src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Notfound(props) {
+    return (
+        <>
+            Ничего не найдено
+        </>
+    )
+}
+
+export default Notfound;
